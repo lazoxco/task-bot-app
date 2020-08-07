192010b50e0616a25d22246fecd18a2ca8a85305
github username <github email address>
2020-08-07 12:13:20 -0400

    Seeded DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Robot.create(name: "Gigabit", slogan: "Only my army of killer robots understands me..")
+Robot.create(name: "Rob Bitt", slogan: "I am not a simple Computer.")
+Robot.create(name: "Screwie", slogan: "I'm still in beta..")
+Robot.create(name: "Brobot", slogan: "Optimus Primates RULE!")
+Robot.create(name: "Databorg", slogan: "I am borg.")
