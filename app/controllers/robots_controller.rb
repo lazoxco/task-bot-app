c9b91e3c133f9843e93d9aa549d616157acd474d
github username <github email address>
2020-08-07 14:15:41 -0400

    App complete.

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,11 +1,12 @@
 class RobotsController < ApplicationController
+  before_action :current_robot, only: [:show, :edit, :update, :destroy]
 
   def index
     @robots = Robot.all
   end
 
   def show
-    @robot = Robot.find(params[:id])
+    # @robot = Robot.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class RobotsController < ApplicationController
   end
 
   def create
-    @robot = Robot.new(robot_params)
+    @robot = Robot.create(robot_params)
     if @robot.save
       redirect_to robot_path(@robot)
     else
@@ -21,9 +22,31 @@ class RobotsController < ApplicationController
     end
   end
 
+  def edit
+    # @robot = Robot.find(params[:id])
+  end
+
+  def update
+    # @robot = Robot.find(params[:id])
+    @robot.update(robot_params)
+
+    redirect_to robot_path(@robot)
+  end
+
+  def destroy
+    # @robot = Robot.find(params[:id])
+    @robot.destroy
+
+    redirect_to robots_path
+  end
+
   private
 
   def robot_params
     params.require(:robot).permit(:name, :slogan)
+  end
+
+  def current_robot
+    @robot = Robot.find(params[:id])
   end
 end
